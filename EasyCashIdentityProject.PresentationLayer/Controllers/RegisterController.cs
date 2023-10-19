719daca188cb8adf22e5ab80cf5eee810adb3b2a
Cem Koç <cem-koc@outlook.com>
2023-10-19 23:29:25 +0300

    Identity Register

--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -36,6 +36,13 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
                 {
                     return RedirectToAction("Index","ConfirmMail");
                 }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
             }
             return View();
         }
